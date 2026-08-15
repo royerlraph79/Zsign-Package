ce9abea026c25c3df0b489cb4175f7448868be4e
Raphaël Royer-Legault <30907279+royerlraph79@users.noreply.github.com>
2026-08-14 22:56:29 -0400

    Refactor Package.swift to include ZsignSwift target

--- Package.swift
+++ Package.swift
@@ -5,72 +5,82 @@ import PackageDescription
 
 let package = Package(
     name: "Zsign",
-	platforms: [
-		.iOS(.v12),
-		.macOS(.v10_15),
-		.tvOS(.v12),
-		.watchOS(.v8),
-		.custom("xros", versionString: "1.3")
-	],
-	products: [
-		.library(
-			name: "zsignc",
-			targets: ["ZsignC"]
-		),
-		.library(
-			name: "Zsign",
-			targets: ["Zsign"]
-		),
-	],
-	dependencies: [
-		.package(url: "https://github.com/krzyzanowskim/OpenSSL", from: "3.3.3001")
-	],
-	targets: [
-		.target(
-			name: "ZsignC",
-			dependencies: [
-				.product(name: "OpenSSL", package: "OpenSSL")
-			],
-			path: "src",
-			exclude: [
-				"common/archive.cpp",
-				"zsign.cpp"
-			],
-			sources: [
-				"archo.cpp",
-				"bundle.cpp",
-				"macho.cpp",
-				"openssl.cpp",
-				"../swift/utils.mm",
-				"signing.cpp",
-				"../swift/zsign.mm",
-				"common/base64.cpp",
-				"common/fs.cpp",
-				"common/json.cpp",
-				"common/log.cpp",
-				"common/sha.cpp",
-				"common/timer.cpp",
-				"common/util.cpp"
-			],
-			publicHeadersPath: "include",
-			cxxSettings: [
-				.headerSearchPath("."),
-				.headerSearchPath("common"),
-				.unsafeFlags(["-std=c++17"])
-			],
-			linkerSettings: [
-				.linkedFramework("OpenSSL"),
-			]
-		),
-		.target(
-			name: "Zsign",
-			dependencies: [
-				"ZsignC"
-			],
-			path: "swift",
-			sources: [
-				"zsign.swift"
-			]
-		)
-	]
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15),
+        .tvOS(.v12),
+        .watchOS(.v8),
+        .custom("xros", versionString: "1.3")
+    ],
+    products: [
+        .library(
+            name: "zsignc",
+            targets: ["ZsignC"]
+        ),
+        .library(
+            name: "Zsign",
+            targets: ["ZsignSwift"]
+        ),
+        .library(
+            name: "ZsignSwift",
+            targets: ["ZsignSwift"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/krzyzanowskim/OpenSSL",
+            from: "3.3.3001"
+        )
+    ],
+    targets: [
+        .target(
+            name: "ZsignC",
+            dependencies: [
+                .product(
+                    name: "OpenSSL",
+                    package: "OpenSSL"
+                )
+            ],
+            path: "src",
+            exclude: [
+                "common/archive.cpp",
+                "zsign.cpp"
+            ],
+            sources: [
+                "archo.cpp",
+                "bundle.cpp",
+                "macho.cpp",
+                "openssl.cpp",
+                "../swift/utils.mm",
+                "signing.cpp",
+                "../swift/zsign.mm",
+                "common/base64.cpp",
+                "common/fs.cpp",
+                "common/json.cpp",
+                "common/log.cpp",
+                "common/sha.cpp",
+                "common/timer.cpp",
+                "common/util.cpp"
+            ],
+            publicHeadersPath: "include",
+            cxxSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("common"),
+                .unsafeFlags(["-std=c++17"])
+            ],
+            linkerSettings: [
+                .linkedFramework("OpenSSL"),
+            ]
+        ),
+        .target(
+            name: "ZsignSwift",
+            dependencies: [
+                "ZsignC"
+            ],
+            path: "swift",
+            sources: [
+                "zsign.swift"
+            ]
+        )
+    ]
 )
